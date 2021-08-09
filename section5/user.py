f8970779c90efe29bb7b6e6aa3b82771aa785773
Tejas Parmar <Tejas.Parmar@contractor.p66.com>
2021-08-09 18:26:44 +0530

    [update] reorganized code

--- section5/user.py
+++ section5/user.py
@@ -1,5 +1,8 @@
 import sqlite3
-class User:
+from flask_restful import Resource, reqparse
+class User():
+	TABLE_NAME = 'users'
+
 	def __init__(self, _id, username, password):
 		self.id = _id
 		self.username = username
@@ -10,19 +13,15 @@ class User:
 		connection = sqlite3.connect('data.db')
 		cursor = connection.cursor()
 
-		query = "SELECT * FROM users WHERE username=?"
-
-		result = cursor.execute(query, (username,))
-
+		query = "SELECT * FROM {table} WHERE username=?".format(table=cls.TABLE_NAME)
+		result = cursor.execute(query, (username, ))
 		row = result.fetchone()
 		if row:
-			#user = cls(row[0], row[1], row[2])
 			user = cls(*row)
 		else:
 			user = None
 
 		connection.close()
-
 		return user
 
 	@classmethod
@@ -30,17 +29,38 @@ class User:
 		connection = sqlite3.connect('data.db')
 		cursor = connection.cursor()
 
-		query = "SELECT * FROM users WHERE id=?"
-
+		query = "SELECT * FROM {table} WHERE id=?".format(table=cls.TABLE_NAME)
 		result = cursor.execute(query, (_id, ))
-
 		row = result.fetchone()
 		if row:
-			#user = cls(row[0], row[1], row[2])
 			user = cls(*row)
 		else:
 			user = None
 
 		connection.close()
-
 		return user
+
+
+class UserRegister(Resource):
+	TABLE_NAME = 'users'
+
+	parser = reqparse.RequestParser()
+	parser.add_argument('username', type=str, required=True, help="This field cannot be left blank!")
+	parser.add_argument('password', type=str, required=True, help="This field cannot be left blank!")
+
+	def post(self):
+		data = UserRegister.parser.parse_args()
+
+		if User.find_by_username(data['username']):
+			return {"message": "User with that username already exists."}, 400
+
+		connection = sqlite3.connect('data.db')
+		cursor = connection.cursor()
+
+		query = "INSERT INTO {table} VALUES (NULL, ?, ?)".format(table=self.TABLE_NAME)
+		cursor.execute(query, (data['username'], data['password']))
+
+		connection.commit()
+		connection.close()
+
+		return {"message": "User created successfully."}, 201
