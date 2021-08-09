be4bbf60c6cd2f97b967ea774c471322f8780a63
Tejas Parmar <Tejas.Parmar@contractor.p66.com>
2021-08-09 14:23:36 +0530

    [update] fixed imports

--- section4/security.py
+++ section4/security.py
@@ -34,7 +34,7 @@ userid_mapping = {u.id: u for u in users}
 
 def authenticate(username, password):
 	user = username_mapping.get(username, None)
-	if user and safe_str_cmp(user['password'], password):
+	if user and safe_str_cmp(user.password, password):
 		return user
 
 
